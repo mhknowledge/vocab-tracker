d0a72c257164cabcad6ec16e0d9e968007f47043
mhknowledge <79174643+mhknowledge@users.noreply.github.com>
2021-08-11 21:11:06 -0400

    index.js models

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(logger("dev"));
 
 mongoose.connect(
     process.env.MONGODB_URI ||'mongodb://localhost/'language',
@@ -21,7 +22,7 @@ mongoose.connect(
 
 //routes
 app.use(require('./routes/api.js'));
-app.use(require('./routes/view.js'));
+app.use(require('./routes/html.js'));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
