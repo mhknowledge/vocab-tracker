b65ef3b1a45affc5475af6ede38d307a3fdeea05
mhknowledge <79174643+mhknowledge@users.noreply.github.com>
2021-08-11 21:38:35 -0400

    Language.js model

---
+++ models/Language.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const LanguageSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    activities: [
+        {
+            type: {
+                type: String,
+                trim: true,
+            },
+            name: {
+                type: String,
+                trim: true,
+            },
+            duration: Number,
+            words: {
+                type: Number,
+                default: 0
+            },
+            reps: {
+                type: Number,
+                default: 0
+            },
+            pages: {
+                type: Number,
+                default: 0
+            }
+        }
+    ],
+    totalDuration: {
+        type: Number,
+        default: 0,
+    }
+});
+
+const Language = mongoose.model("Language", LanguageSchema);
+
+module.exports = Language;
