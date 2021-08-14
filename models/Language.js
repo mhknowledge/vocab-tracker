be32f731d096d15230a50fbe1c893391d0534b17
mhknowledge <79174643+mhknowledge@users.noreply.github.com>
2021-08-14 14:43:46 -0400

    corrections

--- models/Language.js
+++ models/Language.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const LanguageSchema = new Schema({
+const LanguageSchema = new Schema(
+    {
     day: {
         type: Date,
         default: Date.now
@@ -12,13 +13,19 @@ const LanguageSchema = new Schema({
             type: {
                 type: String,
                 trim: true,
+                required: 'Enter an activity type',
             },
             name: {
                 type: String,
                 trim: true,
+                required: 'Enter an activiy name',
             },
-            duration: Number,
-            words: {
+            duration: {
+                type: Number,
+                required: 'Enter an activity duration in minutes',
+                default: 0,
+           },
+            sentences: {
                 type: Number,
                 default: 0
             },
@@ -30,14 +37,21 @@ const LanguageSchema = new Schema({
                 type: Number,
                 default: 0
             }
+        
         }
     ],
-    totalDuration: {
-        type: Number,
-        default: 0,
+},
+    {
+        toObject: {virtuals: true},
+        toJSON: {virtuals: true},
     }
-});
+ );
+ languageSchema.virtuals("totalDuration").get(function () {
+     return this.exercises.reduce((total, exercise) => {
+         return total + exercise.duration;
+     }, 0);
+ });
 
-const Language = mongoose.model("Language", LanguageSchema);
+const Language = mongoose.model("Language", languageSchema);
 
 module.exports = Language;
