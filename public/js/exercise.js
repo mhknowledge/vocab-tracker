09a0c600f9e7b678c0718447ea8cbc3199ba1d46
mhknowledge <79174643+mhknowledge@users.noreply.github.com>
2021-08-14 07:33:30 -0400

    exercise.js file

---
+++ public/js/exercise.js
@@ -0,0 +1,5 @@
+const activityTypeSelect = document.querySelector("#type");
+const vocabForm = document.querySelector(".vocabulary-form");
+const readingForm = document.querySelector(".reading-form");
+const writingForm = document.querySelector(".writing-form");
+const convoForm = document.querySelector(".converstation-form");
